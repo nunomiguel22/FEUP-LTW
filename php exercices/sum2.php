f1dbb1e373048beedceb3ca1057fbc33e99dc3a2
Nuno Miguel <37627936+nunomiguel22@users.noreply.github.com>
2020-10-16 11:49:42 +0100

    php ex1and2

---
+++ php exercices/sum2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$res = $num1 + $num2;
+echo $res
+?>
+
+
+<form action="/~up201708997/form2.html">
+    <input type="submit" value="Back to form">
+</form>
